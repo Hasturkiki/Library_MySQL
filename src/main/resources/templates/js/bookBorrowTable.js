1a9767ea5a7d79b92e4531aa60116f2b20966c97
Hastur Kiki <yy1970790674@gmail.com>
2022-09-06 21:23:09 +0800

    修改首页样式 完善tagHome之外的主表首页面及初次加载功能 修改了数据库 增添了借书表和共同作者表的Vo类 （补提交bookBorrowTable.js）

--- src/main/resources/templates/js/bookBorrowTable.js
+++ src/main/resources/templates/js/bookBorrowTable.js
@@ -1,30 +1,59 @@
-window.onload = function getAllTag() {
-    let tagTable = document.getElementsByClassName("tag_table")[0];
-    myAxios.get('/tag/getAll').then(res => {
+window.onload = function getAllBookBorrowTable() {
+    let bookBorrowTableTable = document.getElementsByClassName("bookBorrowTable_table")[0];
+    myAxios.get('/bookBorrowTable/getAll').then(res => {
         if (res.code === 200) {
-            let tagList = res.data
-            if (tagList.length !== 0) {
-                tagTable.innerHTML = '<colgroup>\n' +
+            let bookBorrowTableVoList = res.data
+            if (bookBorrowTableVoList.length !== 0) {
+                bookBorrowTableTable.innerHTML = '<colgroup>\n' +
                     '            <col style="background-color: #bcc">\n' +
-                    '            <col style="background-color: #cdd">\n' +
+                    '            <col span="5" style="background-color: #cdd">\n' +
                     '            <col style="background-color: #dee">\n' +
                     '        </colgroup>\n' +
                     '        <tr>\n' +
-                    '            <th>标签ID</th>\n' +
-                    '            <th>标签名称</th>\n' +
+                    '            <th>借书表ID</th>\n' +
+                    '            <th>借书者</th>\n' +
+                    '            <th>所借书</th>\n' +
+                    '            <th>借书状态</th>\n' +
+                    '            <th>外借时间</th>\n' +
+                    '            <th>归还时间</th>\n' +
                     '            <th>操作</th>\n' +
                     '        </tr>'
-                for (const tag of tagList) {
+                for (const bookBorrowTableVo of bookBorrowTableVoList) {
                     let tr = document.createElement("tr");
                     tr.className = "search_result_content";
 
-                    let td_tagId = document.createElement("td");
-                    td_tagId.innerText = tag.tagId;
+                    const bookBorrowTable = bookBorrowTableVo.bookBorrowTable
 
-                    let td_tagName = document.createElement("td");
-                    td_tagName.innerText = tag.tagName;
+                    let td_bookBorrowTableId = document.createElement("td");
+                    td_bookBorrowTableId.innerText = bookBorrowTable.booksBorrowTableId
 
-                    let td_operate = document.createElement("td");
+                    let td_readerId = document.createElement("td");
+                    td_readerId.innerText = bookBorrowTableVo.readerName
+                    td_readerId.readerId = bookBorrowTable.readerId
+
+                    let td_bookId = document.createElement("td");
+                    td_bookId.innerText = bookBorrowTableVo.bookName
+                    td_bookId.bookId = bookBorrowTable.bookId
+
+                    let td_isBorrowing = document.createElement("td");
+                    if (bookBorrowTable.isBorrowing === 1) {
+                        td_isBorrowing.innerText = '借用中'
+                    } else {
+                        td_isBorrowing.innerText = '已归还'
+                    }
+
+
+                    let td_borrowTime = document.createElement("td");
+                    td_borrowTime.innerText = bookBorrowTable.borrowTime.split('T')[0] + ' ' + bookBorrowTable.borrowTime.split('T')[1]
+
+                    let td_backTime = document.createElement("td");
+                    if(bookBorrowTable.backTime === null) {
+                        td_backTime.innerText = '无'
+                    } else {
+                        td_backTime.innerText = bookBorrowTable.borrowTime.split('T')[0] + ' ' + bookBorrowTable.borrowTime.split('T')[1]
+                    }
+
+                    let td_operate = document.createElement("td")
                     let button_update = document.createElement('button')
                     button_update.innerText = '编辑'
                     button_update.className = 'table_button_update'
@@ -34,31 +63,36 @@ window.onload = function getAllTag() {
                     td_operate.appendChild(button_update)
                     td_operate.appendChild(button_delete)
 
-                    tr.appendChild(td_tagId);
-                    tr.appendChild(td_tagName);
+                    tr.appendChild(td_bookBorrowTableId);
+                    tr.appendChild(td_readerId);
+                    tr.appendChild(td_bookId);
+                    tr.appendChild(td_isBorrowing);
+                    tr.appendChild(td_borrowTime);
+                    tr.appendChild(td_backTime);
                     tr.appendChild(td_operate);
 
-                    tagTable.appendChild(tr)
+                    bookBorrowTableTable.appendChild(tr)
                 }
             } else {
-                let p = document.createElement("p");
-                p.className = "search_result_emptyHind";
-                p.innerText = "无对应内容，请确认后重试。";
-                tagTable.appendChild(p);
+                let p = document.createElement("p")
+                p.className = "search_result_emptyHind"
+                p.innerText = "无对应内容，请确认后重试。"
+                bookBorrowTableTable.appendChild(p)
             }
         } else {
-            let p = document.createElement("p");
-            p.className = "search_result_emptyHind";
-            p.innerText = "无对应内容，请确认后重试。";
-            tagTable.appendChild(p);
+            let p = document.createElement("p")
+            p.className = "search_result_emptyHind"
+            p.innerText = res.msg.split(';')[0]
+            console.log(res.msg.split(';')[1])
+            bookBorrowTableTable.appendChild(p)
         }
     })
 }
 
 $('.table_button_update').click({
-//    todo update tag
+//    todo update bookBorrowTable
 })
 
 $('.table_button_delete').click({
-//    todo delete tag
+//    todo delete bookBorrowTable
 })
