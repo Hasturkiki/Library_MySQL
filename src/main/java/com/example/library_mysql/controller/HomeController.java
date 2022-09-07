10045fc4d69523524c0c0eb2df82760e27be843e
Hastur Kiki <yy1970790674@gmail.com>
2022-09-07 23:58:24 +0800

    修改主页搜索功能 新增条目链接（下一步完善链接跳转页面和方法） 微调部分样式 暂时修复控制台中文显示问题及使显示更具逻辑性 （补提交HomeController）

--- src/main/java/com/example/library_mysql/controller/HomeController.java
+++ src/main/java/com/example/library_mysql/controller/HomeController.java
@@ -15,8 +15,7 @@ import javax.annotation.Resource;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
-
-import static java.lang.Integer.parseInt;
+import java.util.Objects;
 
 @Api(tags = "首页")
 @Controller
@@ -38,28 +37,50 @@ public class HomeController {
     @Resource
     private TagService tagService;
 
+    @SuppressWarnings("rawtypes")
     @ResponseBody
     @PostMapping("/search")
     @ApiOperation("首页搜索")
-    @ApiImplicitParam(name = "searchValue", value = "搜索关键词", required = true, paramType = "query", dataType = "String")
-    public R<Object> show(String searchValue) throws UnsupportedEncodingException {
-        if (searchValue != null) {
-            int id = parseInt(searchValue);
-            List<Object> result = new ArrayList<>();
-            Author author = authorService.selectAuthorById(id);
-            BookVo bookVo = bookService.selectBookVoById(id);
-            PublishingCompany publishingCompany = publishingCompanyService.selectPublishingCompanyById(id);
-            Reader reader = readerService.selectReaderById(id);
-            Tag tag = tagService.selectTagById(id);
+    @ApiImplicitParam(name = "searchKey", value = "搜索关键词", required = true, paramType = "query", dataType = "String")
+    public R<Object> show(String searchKey) throws UnsupportedEncodingException {
+        if (searchKey != null) {
+            List<List> result = new ArrayList<>();
+            List<Author> authorList = authorService.searchByName(searchKey);
+            List<BookVo> bookVoList = bookService.searchByName(searchKey);
+            List<PublishingCompany> publishingCompanyList = publishingCompanyService.searchByName(searchKey);
+            List<Reader> readerList = readerService.searchByName(searchKey);
+            List<Tag> tagList = tagService.searchByName(searchKey);
 
-            result.add(author);
-            result.add(bookVo);
-            result.add(publishingCompany);
-            result.add(reader);
-            result.add(tag);
+            result.add(authorList);
+            result.add(bookVoList);
+            result.add(publishingCompanyList);
+            result.add(readerList);
+            result.add(tagList);
 
-            String str = new String(result.toString().getBytes(), "GBK");
-            System.out.println("searchValue " + searchValue + " success.\n" + str);
+            //to_do 修复中文显示问题 （尚未完全解决，clean后会无法运行，需重设项目编码为UTF-8后编译一次再重设为GBK才能正确显示）
+            System.out.println("\nsearch '" + new String(searchKey.getBytes(), "GBK") + "' success.");
+            if (result.stream().anyMatch(Objects::nonNull)) {
+                System.out.println("result: {");
+                int i = 0;
+                String[] strings = {"Author", "Book", "PublishingCompany", "Reader", "Tag"};
+                for (List objectList : result) {
+                    if (objectList == null)
+                        System.out.println("\t" + strings[i] + ": null");
+                    else {
+                        System.out.println("\t" + strings[i] + ": ");
+                        for (Object object : objectList) {
+                            String str = object.toString().split("[(]", 2)[1]
+                                    .replaceFirst(".$", "").replace("=",": ");
+                            if (str.split("[(]").length == 2)
+                                str = str.split("[(]")[1].replace(")","");
+                            System.out.println("\t\t[" + str+"]");
+                        }
+                    }
+                    i++;
+                }
+                System.out.println("}");
+            } else
+                System.out.println("result: null");
             return R.success(result);
         } else {
             System.out.println("search fail.");
